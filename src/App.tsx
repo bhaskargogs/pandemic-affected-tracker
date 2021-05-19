daf12ddd3918229e20d17d20eddaf7f8debd5d39
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-05-19 09:02:57 +0200

    Updated environment and app

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://github.com/EliEladElrom/react-tutorials" target="_blank" rel="noopener noreferrer">
-          Eli Elad Elrom - React Tutorials
-        </a>
+        <div>{process.env.REACT_APP_API_URL}</div>
       </header>
     </div>
   )
